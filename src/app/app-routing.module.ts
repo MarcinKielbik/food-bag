e9401812e11ff3bec0f622210dac72d7582df6ff
Marcin Kielbik <marcinlukasz.kielbik@gmail.com>
2023-04-19 20:35:18 +0200

    initial commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -7,7 +7,7 @@ import { LoginComponent } from './login/login.component';
 
 const routes: Routes = [
   {path: "home", component: HomeComponent},
-  {path:"", redirectTo:"home",pathMatch:"full"},
+  {path:"/", redirectTo:"home",pathMatch:"full"},
   {path: "produkty", component: ProductsComponent},
   {path: "kontakt", component: ContactComponent},
   {path: "logowanie", component: LoginComponent},
